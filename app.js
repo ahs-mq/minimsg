0d0d8b1d62f5128a5a3e7c0f80a6cdfa0e14b27c
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2025-09-21 19:37:33 +0400

    completed submit form func.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const PORT = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
+//express.urlencoded() is a method inbuilt in express to recognize the incoming Request Object as strings or arrays
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    const messages = [
+
+const messages = [
   {
     text: "Hi there!",
     user: "Amando",
@@ -20,11 +22,25 @@ app.get('/', (req, res) => {
   }
 ];
 
+app.get('/', (req, res) => {
+
     res.render("index", { title: "Mini Messageboard", messages: messages })
 });
 
+//render new page first
 app.get('/new', (req,res)=>{
+    console.log(req.params)
     res.render('new')
 })
+//receive submitted data
+app.post('/new', (req, res) => {
+  const { user, text } = req.body; // Extract values from the form
+  req.body.added = new Date();     // Add timestamp to the object
+  messages.push(req.body);         // Store the updated object
+
+  console.log(`Username: ${user}, Text: ${text}`);
+  res.send(`Received: ${user}, ${text}. Back to <a href="/">Home</a>`);
+});
+
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
